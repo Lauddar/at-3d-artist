20ecf17a86e38c5e1371380f45ebcbf6157d617d
laurafive <laura.garcia@fiveisblue.com>
2023-08-30 09:41:09 +0200

    fix homeBanner and title

--- app/components/home/initial-section/HomeBanner.jsx
+++ app/components/home/initial-section/HomeBanner.jsx
@@ -8,15 +8,12 @@ export function HomeBanner({ title, subtitle, description }) {
     return (
         <>
             <div className="h-[1700px] w-full absolute top-0 left-0 -z-50 bg-secondary"></div>
-            <section className="h-[1700px] absolute top-0 margin-default">
-                <div className="flex justify-between sticky top-24">
-                    <div className="pl-16 pt-28 w-[600px] mb-28"><Scene /></div>
-                    <div className='flex flex-col items-end text-white w-1/2 mr-10 text-right title-position'>
-                        <p className='text-lg w-2/3 leading-normal'>{description}</p>
+            <section className="h-[1700px] w-full absolute top-0 right-0 padding-default">
+                    <div className='fixed right-24 text-white text-right title-position'>
+                        <p className='text-lg w-1/3 leading-normal mr-0 ml-auto'>{description}</p>
                         <h1 className={`text-9xl uppercase leading-0.8 ${changaOne.className}`}>{title}</h1>
                         <h2 className={`text-7xl uppercase leading-0.8 ${changaOne.className}`}>{subtitle}</h2>
                     </div>
-                </div>
             </section >
         </>
     )
--- app/page.js
+++ app/page.js
@@ -6,9 +6,15 @@ import { HomeBanner } from './components/home/initial-section/HomeBanner'
 import { useState } from "react";
 
 export default function Home() {
+  const homeBannerProps = {
+    title: 'Albert Toll',
+    subtitle: '3D Artist',
+    description: 'Lorem ipsum dolor sit amet consectetur. Quis eu semper imperdiet mi. Quis in convallis mi augue lorem id. Pellentesque sit fermentum eget non interdum feugiat nisl congue. Sed interdum tortor consequat nullam non fermentum mauris.'
+  }
+
   return (
     <main className="main-position">
-      <HomeBanner title='Albert Toll' subtitle='3D Artist' description='Lorem ipsum dolor sit amet consectetur. Quis eu semper imperdiet mi. Quis in convallis mi augue lorem id. Pellentesque sit fermentum eget non interdum feugiat nisl congue. Sed interdum tortor consequat nullam non fermentum mauris.' />
+      <HomeBanner {...homeBannerProps} />
     </main>
   )
 }
