f6cd8bed5c092ac9637ca8e04832e68a6fa1e815
laurafive <laura.garcia@fiveisblue.com>
2023-09-01 17:11:06 +0200

    coses que estan malament

--- app/components/3d-assets/3d-scenes/CapsuleScene.jsx
+++ app/components/3d-assets/3d-scenes/CapsuleScene.jsx
@@ -1,12 +1,15 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Canvas, useThree } from "@react-three/fiber";
-import { OrbitControls, Enviroment, useGLTF, PerspectiveCamera } from '@react-three/drei'
 import { Capsule } from "../3d-models/Capsule"
+import { TrackballControls } from "@react-three/drei";
+
 
 export function CapsuleScene() {
+  
   return (
     <Canvas>
       <Capsule />
+      <TrackballControls />
     </Canvas>
   );
 }
