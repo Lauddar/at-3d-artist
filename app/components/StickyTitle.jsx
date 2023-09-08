5f2e372c8bbb66d4a55d98e5017877731da4136a
laurafive <laura.garcia@fiveisblue.com>
2023-09-08 09:55:04 +0200

    add title transition

--- app/components/StickyTitle.jsx
+++ app/components/StickyTitle.jsx
@@ -5,15 +5,12 @@ const changaOne = Changa_One({ subsets: ['latin'], weight: '400' })
 
 export function StickyTitle({ title, subtitle }) {
     const [isVisible, setIsVisible] = useState(false);
-    let scrollValue = 800;
+    let scrollValue = 750;
 
     useEffect(() => {
-        // Función para manejar el evento de scroll
         function handleScroll() {
-            // Obtener la posición actual de desplazamiento vertical
             const scrollY = window.scrollY;
 
-            // Verificar si el scroll ha pasado el 100%
             if (scrollY >= scrollValue) {
                 setIsVisible(true);
             } else {
@@ -29,10 +26,7 @@ export function StickyTitle({ title, subtitle }) {
     }, []);
 
     return (
-        <div
-            className={`fixed text-end text-secondary mt-2 top-28 mr-10 d-right ${isVisible ? "visible" : "hidden"
-                }`}
-        >
+        <div className={`fixed text-end text-secondary mt-2 top-28 mr-10 d-right -z-20 ${isVisible ? "visible" : "hidden"}`}>
             <h1 className={`text-5xl uppercase leading-0.8 ${changaOne.className}`}>
                 {title}
             </h1>
--- app/components/home/initial-section/HomeBannerContent.jsx
+++ app/components/home/initial-section/HomeBannerContent.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react';
 import { useSpring, animated } from "react-spring";
-import { Changa_One } from 'next/font/google';
+import { Changa_One, Righteous } from 'next/font/google';
 
 const changaOne = Changa_One({ subsets: ['latin'], weight: '400' });
 
 export function HomeBannerContent({ title, subtitle, description }) {
     const [scrollY, setScrollY] = useState(0);
+    const [isVisible, setIsVisible] = useState(true);
+    let scrollValue = 800;
 
     useEffect(() => {
         const handleScroll = () => {
-            setScrollY(window.scrollY);
+            const newScrollY = window.scrollY;
+            setScrollY(newScrollY);
+            setIsVisible(newScrollY < scrollValue);
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -20,28 +24,31 @@ export function HomeBannerContent({ title, subtitle, description }) {
     }, []);
 
     const calcScale = (y) => {
-        return 1 - y * 0.001;
+        if (y >= 850) {
+            return 0.36;
+        } else {
+            return 1 - (y / 850) * (1 - 0.36);
+        }
     };
 
     const { scale } = useSpring({
         scale: calcScale(scrollY),
-        config: { tension: 120, friction: 14 },
+        config: { tension: 500, friction: 50 },
     });
 
-    const scaleValue = scale.interpolate((s) => `scale(${s})`);
-
-    console.log(scaleValue)
+    const scaleValue = scale.to((s) => `scale(${s})`);
 
     return (
-        <section className="pt-80 h-130">
-            <div className="sticky mt-2 top-28 mr-10 text-blue-900 text-right scroll-element"
+        <section className={`pt-80 h-130 ${isVisible ? "visible" : "hidden"}`}>
+            <animated.div className="sticky mt-2 top-28 mr-10 text-white text-right scroll-element"
                 style={{
                     transform: scaleValue,
+                    transformOrigin: 'top right',
                 }}>
                 <p className="mr-0 ml-auto w-[540px] text-lg">{description}</p>
                 <h1 className={`text-9xl uppercase leading-0.8 ${changaOne.className}`}>{title}</h1>
                 <h2 className={`text-7xl uppercase leading-0.8 ${changaOne.className}`}>{subtitle}</h2>
-            </div>
+            </animated.div>
         </section>
     );
 }
